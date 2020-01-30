434e399141318c1e28afbc309016596b1080049a
Antchel <golan349@gmail.com>
2020-01-30 23:03:58 +0500

    Realize transl. w/o algorithm

--- lib.h
+++ lib.h
@@ -8,6 +8,7 @@
 #include <iostream>
 using std::string;
 using std::cout;
+using std::cin;
 using std::endl;
 
 #endif //TASK_TO_SOLVE_LIB_H
--- main.cpp
+++ main.cpp
@@ -2,15 +2,36 @@
 // Roman to integer translation
 //
 #include "lib.h"
+#include <map>
 class Solution {
 public:
-    int romanToInt(string s) {
-
+    int romanToInt(string const &s)
+    {
+        for (char i : s)
+        {
+            result += Roman[i];
+        }
+        return result;
     }
+private:
+    int result = 0;
+    std::map <char,int> Roman = {{'M',1000},
+                                 {'D',500},
+                                 {'C',100},
+                                 {'L',50},
+                                 {'X',10},
+                                 {'V',5},
+                                 {'I',1},
+                                 };
 };
 
 int main ()
 {
-    cout << "Initial commit"<<endl;
+    uint32_t IntNum;
+    string RomanNum;
+    Solution translateToint;
+    cin >> RomanNum;
+    IntNum = translateToint.romanToInt(RomanNum);
+    cout << "Roman " << RomanNum <<  " euqal is "<< IntNum << endl;
     return EXIT_SUCCESS;
 }
