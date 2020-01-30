7c5dc9b5e03a734d0abe2ca69d798058c72ad230
Antchel <golan349@gmail.com>
2020-01-30 22:32:17 +0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/task_to_solve.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task_to_solve.dir/main.cpp.o"
+  "task_to_solve"
+  "task_to_solve.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task_to_solve.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by anton on 30.01.2020.
+//
+#include <iostream>
+using std::cout;
+using std::endl;
+int main ()
+{
+    cout << "Initial commit"<<endl;
+    return EXIT_SUCCESS;
+}
