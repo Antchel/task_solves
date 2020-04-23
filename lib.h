baf901b9b2849f8193e92aabf5070b1dfeac2061
Antchel <golan349@gmail.com>
2020-04-23 23:44:03 +0500

    Int to binary parser

--- lib.h
+++ lib.h
@@ -1,14 +1,47 @@
-//
-// Created by anton on 30.01.2020.
-//
 
-#ifndef TASK_TO_SOLVE_LIB_H
-#define TASK_TO_SOLVE_LIB_H
-#include <string>
 #include <iostream>
-using std::string;
-using std::cout;
-using std::cin;
-using std::endl;
+#include <string>
+#include <climits>
+#include <limits>
 
-#endif //TASK_TO_SOLVE_LIB_H
+#define LOG(M, ...) printf("\n" M "\n", ## __VA_ARGS__)
+
+using namespace std;
+
+const int Error_OutOfIntRange = 10;
+
+template <typename T>
+void IntToBinary(T value)
+{
+    string buf, result;
+    uint8_t base = 2;
+    bool _isNegative = false;
+
+    if ((value < numeric_limits<int>::min()) || (value > numeric_limits<int>::max()))
+        throw Error_OutOfIntRange;
+    if (value == numeric_limits<int>::min())
+    {
+        result = string("-1") + string(sizeof(int) * CHAR_BIT - 1, '0');
+        goto out;
+    }
+
+    if (value < 0)
+    {
+        value*=-1;
+        _isNegative = true;
+    }
+
+    do
+    {
+        buf += ('0' + (value%base));
+        value/=base;
+
+    } while (value > 0);
+
+    for (int i = buf.size() - 1; i >= 0; i--)
+        result += buf[i];
+    if (_isNegative)
+        result = "-" + result;
+    out:
+    cout << result << endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,43 +1,17 @@
-//
-// Roman to integer translation
-//
 #include "lib.h"
-#include <map>
-#include <vector>
-#include <algorithm>
-using std::vector;
-class Solution {
-        public:
-        int oddCells(const int n,const int m, vector<vector<int>>& indices) {
-            int **matrix = new int* [m];
-            for (uint8_t i = 0; i < n; i++)
-            {
-                matrix[i] = new int [n];
-            }
-            for (uint8_t i = 0; i < n; i++)
-            {
-                for (uint8_t j = 0; j < m; j++)
-                {
-                    matrix [i][j] = rand()%20;
-                }
-            }
-            for (uint8_t i = 0; i < n; i++)
-            {
-                for (uint8_t j = 0; j < m; j++)
-                {
-                    cout << matrix [i][j] << '\t';
-                }
-                cout <<endl;
-            }
-            return 5;
-        }
-};
 
-int main ()
+int main()
 {
-    vector<vector<int>> indices1 = {{0,1},
-                                    {1,2}};
-    Solution odd;
-    odd.oddCells(5,4,indices1);
-    return EXIT_SUCCESS;
+    LOG("\tStart testing Int to binary convertation\n");
+    try {
+        IntToBinary(2);
+        IntToBinary(numeric_limits<int>::max());
+        IntToBinary(numeric_limits<int>::min());
+        IntToBinary(500000000000);
+    }
+    catch (int num) {
+        cout << "Error " << num << "\tOut of Int range" << endl;
+    }
+
+    LOG("\tFinish testing Start testing Int to binary convertation\n");
 }
